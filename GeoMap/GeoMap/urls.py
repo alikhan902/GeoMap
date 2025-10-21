8ddfbc57671a08406fe6b7c4be738b5fd0aaaa92
Urgot <madaevgerehan@gmail.com>
2025-10-21 15:05:26 +0300

    add blank start page

--- GeoMap/GeoMap/urls.py
+++ GeoMap/GeoMap/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Здесь будет карта")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index)
 ]
